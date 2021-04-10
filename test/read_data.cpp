ea043b65f11ddaf5b6ce7f06422e85674c7978e9
Pierrelean @bolshoyshlyoppa aka Leonid Perlin Vadimovich <perlinleo@gmail.com>
2021-04-10 19:17:00 +0300

    travis 5

--- test/read_data.cpp
+++ test/read_data.cpp
@@ -9,20 +9,20 @@ extern "C"{
 
 TEST(getdocs, correct){
     hashtable_t *TF_ALL_DOCS = create_hash_table();
-    int a = get_tf_idf_from_dir("../../data/test-1/",TF_ALL_DOCS);
+    int a = get_tf_idf_from_dir("../data/test-1/",TF_ALL_DOCS);
     ASSERT_EQ(a,5);
 }
 
 TEST(decodeAndPrint, correct){
     hashtable_t *TF_ALL_DOCS = create_hash_table();
-    int a = get_tf_idf_from_dir("../../data/test-1/",TF_ALL_DOCS);
+    int a = get_tf_idf_from_dir("../data/test-1/",TF_ALL_DOCS);
     ASSERT_EQ(a,5);
 }
 
 
 TEST(top5, correctdata1){
   hashtable_t *TF_ALL_DOCS = create_hash_table();
-  int a = get_tf_idf_from_dir("../../data/test-1/",TF_ALL_DOCS);
+  int a = get_tf_idf_from_dir("../data/test-1/",TF_ALL_DOCS);
    ASSERT_EQ(a,5);
    int b = get_top_5_for_doc(TF_ALL_DOCS,"1");
    ASSERT_EQ(b,6);
